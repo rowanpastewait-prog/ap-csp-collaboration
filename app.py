245941a90452be76ec18e47c89dfc509da2650c2
rowanpastewait-prog <rowanpastewait@gmail.com>
2026-06-03 16:49:59 -0400

    All grade menus

--- app.py
+++ app.py
@@ -1,3 +1,135 @@
 Amenu=Group(
-    Rect(0,0,400,400)
-)
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade A: 93-100%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 5.0 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 4.5 GPA',200,160, fill='white', size=25),
+    Label('General Class: 4.0', 200,190,fill='white', size=25),
+    Label('Amazing score!!',200,300, fill='white', size=25),
+    )
+Amenu.visible=False
+
+Aminusmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade A-: 90-92%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 4.7 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 4.2 GPA',200,160, fill='white', size=25),
+    Label('General Class: 3.7', 200,190,fill='white', size=25),
+    Label('Amazing score!!',200,300, fill='white', size=25),
+    )
+Aminusmenu.visible=False
+
+Bplusmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade B+: 87-89%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 4.3 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 3.8 GPA',200,160, fill='white', size=25),
+    Label('General Class: 3.3', 200,190,fill='white', size=25),
+    Label('Great Score!',200,300, fill='white', size=25),
+    )
+Bplusmenu.visible=False
+
+Bmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade B: 83-86%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 4.0 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 3.5 GPA',200,160, fill='white', size=25),
+    Label('General Class: 3.0', 200,190,fill='white', size=25),
+    Label('Great Score!',200,300, fill='white', size=25),
+    )
+Bmenu.visible=False
+
+Bminusmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade B-: 80-82%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 3.7 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 3.2 GPA',200,160, fill='white', size=25),
+    Label('General Class: 2.7', 200,190,fill='white', size=25),
+    Label('Good Score!',200,300, fill='white', size=25),
+    )
+Bminusmenu.visible=False
+
+Cplusmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade C+: 77-79%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 3.3 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 2.8 GPA',200,160, fill='white', size=25),
+    Label('General Class: 2.3', 200,190,fill='white', size=25),
+    Label('Good Score!',200,300, fill='white', size=25),
+    )
+Cplusmenu.visible=False
+
+Cmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade C: 73-76%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 3.0 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 2.5 GPA',200,160, fill='white', size=25),
+    Label('General Class: 2.0', 200,190,fill='white', size=25),
+    Label('Decent Score!',200,300, fill='white', size=25),
+    )
+Cmenu.visible=False
+
+Cminusmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade C-: 70-72%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 2.7 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 2.2 GPA',200,160, fill='white', size=25),
+    Label('General Class: 1.7', 200,190,fill='white', size=25),
+    Label('Decent Score!',200,300, fill='white', size=25),
+    )
+Cminusmenu.visible=False
+
+Dplusmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade D+: 67-69%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 1.3 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 1.3 GPA',200,160, fill='white', size=25),
+    Label('General Class: 1.3', 200,190,fill='white', size=25),
+    Label('Bad Score!',200,300, fill='white', size=25),
+    Label('No GPA boost for Honors and AP classes', 200,350, fill='white', size=20),
+    )
+Dplusmenu.visible=False
+
+Dmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade D: 63-66%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 1.0 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 1.0 GPA',200,160, fill='white', size=25),
+    Label('General Class: 1.0', 200,190,fill='white', size=25),
+    Label('Bad Score!',200,300, fill='white', size=25),
+    Label('No GPA boost for Honors and AP classes', 200,350, fill='white', size=20),
+    )
+Dmenu.visible=False
+
+Dminusmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade D-: 60-62%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 0.7 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 0.7 GPA',200,160, fill='white', size=25),
+    Label('General Class: 0.7', 200,190,fill='white', size=25),
+    Label('Bad Score!',200,300, fill='white', size=25),
+    Label('No GPA boost for Honors and AP classes', 200,350, fill='white', size=20),
+    )
+Dmenu.visible=False
+
+Fmenu=Group(
+    Rect(0,0,400,400,fill='black'),
+    Label('Grade F: 0-59%'200,55,fill='white',size=25),
+    Label('GPA for type of class', 200,100, fill='white', size=25),
+    Label('AP Class: 0 GPA', 200,160, fill='white', size=25),
+    Label('Honors Class: 0 GPA',200,160, fill='white', size=25),
+    Label('General Class: 0', 200,190,fill='white', size=25),
+    Label('Failed!',200,300, fill='white', size=25),
+    Label('No GPA boost for Honors and AP classes', 200,350, fill='white', size=20),
+    )
+Fmenu.visible=False
