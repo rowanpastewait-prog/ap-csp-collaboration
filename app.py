ab251e61f9119d3334436acd16c1110093441644
AdamKnotts <adamwknotts@icloud.com>
2026-06-03 17:35:28 -0400

    Written by Adam

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Text with information about each grade
 Amenu=Group(
     Rect(0,0,400,400,fill='black'),
     Label('Grade A: 93-100%'200,55,fill='white',size=25),
@@ -133,19 +134,22 @@ Fmenu=Group(
     Label('No GPA boost for Honors and AP classes', 200,350, fill='white', size=20),
     )
 Fmenu.visible=False
-
+#Background Colors
 app.colors=['lightgray','lightblue','orange','crimson','black']
-
+#Exit button sends you back to home page
 ExitButton=Group(
     Rect(350,11,40,40,fill='Red')
     )
 ExitButton.visible=False
-
+#List
 menus=[Amenu,Aminusmenu,Bplusmenu,Bmenu,Bminusmenu,Cplusmenu,Cmenu,Cminusmenu,Dplusmenu,Dmenu,Dminusmenu,Fmenu]
+#Function 1 changes background to different colors
 def cycleColors(colorList):
     if(Title.visible==False):
+        #Iteration (loop)
         for i in range(len(colorList)):
             currentColor=colorList[i]
+            #Selection Statement (if/else)
             if(currentColor=='lightgray'):
                 colorList[i]='lightblue'
             elif(currentColor=='lightblue'):
@@ -158,3 +162,149 @@ def cycleColors(colorList):
                 colorList[i]='lightgray'
     for menu in menus:
         menu.children[0].fill=app.colors[0]
+
+    #Lines on homepage
+    PaperLines=Group(
+        Line(0,30,400,30,fill='black',opacity=25),
+        Line(0,60,400,60,fill='black',opacity=25),
+        Line(0,90,400,90,fill='black',opacity=25),
+        Line(0,120,400,120,fill='black',opacity=25),
+        Line(0,150,400,150,fill='black',opacity=25),
+        Line(0,180,400,180,fill='black'opacity=25),
+        Line(0,210,400,210,fill='black',opacity=25),
+        Line(0,240,400,240,fill='black',opacity=25),
+        Line(0,270,400,270,fill='black',opacity=25),
+        Line(0,300,400,300,fill='black',opacity=25),
+        Line(0,330,400,330,fill='black',opacity=25),
+        Line(0,360,400,360,fill='black',opacity=25),
+        Line(0,390,400,390,fill-'black',opacity=25),
+    )
+    #Grades
+    Title=Group(
+        Label('Select Grade',200,25,size=50)
+    )
+
+    GPALabel=Group(
+        Label('GPA Calculator',200,75,size=15,font='monspace')
+    )
+
+    GradeALabel=Group(
+        Label('A',100,75,size=50),
+        Rect(82,57,35,35,fill=None),
+    )
+    GradeAminus=Group(
+        Label('A-',300,75,size=50),
+        Rect(280,57,35,35,fill=None),
+    )
+    GradeBplus=Group(
+        Label('B+',100,125,size=50),
+        Rect(70,108,35,35,fill=None),
+    )
+    GradeB=Group(
+        Label('B',300,125,size=50),
+        Rect(280,108,35,35,fill=None),
+    )
+    GradeBminus=Group(
+        Label('B-',100,175,size=50),
+        Rect(70,158,35,35,fill=None),
+    )
+    GradeCplus=Group(
+        Label('C+',300,175,size=50),
+        Rect(270,160,35,35,fill=None),
+    )
+    GradeC=Group(
+        Label('C',100,225,size=50),
+        Rect(80,208,35,35,fill=None),
+    )
+    GradeCminus=Group(
+        Label('C-',300,225,size=50),
+        Rect(280,205,35,35,fill=None),
+    )
+    GradeDplus=Group(
+        Label('D+',100,275,size=50),
+        Rect(70,258,35,35,fill=None),
+    )
+    GradeD=Group(
+        Label('D',300,275,size=50),
+        Rect(280,255,35,35,fill=None),
+    )
+    GradeDminus=Group(
+        Label('D-',100,325,size=50),
+        Rect(70,308,35,35,fill=None),
+    )
+    GradeF=Group(
+        Label('F',300,325,size=50),
+        Rect(280,305,35,35,fill=None),
+    )
+
+    homepage_elements=[
+        PaperLines,Title,GPALabel,GradeA,GradeAminus,GradeBplus,GradeB,GradeBminus,GradeCplus,GradeC,GradeCminus,GradeDplus,GradeD,GradeD,imus,GradeF
+    ]
+    #Function 2 makes homepage elements invisible
+    def hideHomepage():
+        for element in homepage_elements:
+            element.visible=False
+        #Function 3 makes grade menus invisible
+        def hideAllMenus():
+            for menu in menus:
+                menu.visible=False
+
+
+    def onMousePress(mouseX,mouseY):
+        if(Title.visible==False):
+            cycleColors(app.colors)
+            Amenu.visible=True
+
+        if(GradeA.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Amenu.visible=True
+        if(GradeAminus.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Aminusmenu.visible=True
+        if(GradeBplus.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Bplusmenu.visible=True
+        if(GradeB.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Bmenu.visible=True
+        if(GradeBminus.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Bminusmenu.visible=True
+        if(GradeCplus.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Cplusmenu.visible=True
+        if(GradeC.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Cmenu.visible=True
+        if(GradeCminus.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Cminusmenu.visible=True
+        if(GradeDplus.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Dplusmenu.visible=True
+        if(GradeD.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Dmenu.visible=True
+        if(GradeDminus.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Dminusmenu.visible=True
+        if(GradeF.hits(mouseX,mouseY)):
+            hideHomepage()
+            ExitButton.visible=True
+            Fmenu.visible=True
+        if(ExitButton.hits(mouseX,mouseY)):
+            hideAllMenus()
+            ExitButton.visible=True
+            for element in homepage_elements:
+                element.visible=True
